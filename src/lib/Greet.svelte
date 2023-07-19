5fef2a2eff213361ab11a2fc7c7283be72669d04
Adimac93 <adimac93@gmail.com>
2023-07-19 15:54:34 +0200

    💄 add skeleton

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+  import { invoke } from "@tauri-apps/api/tauri";
 
   let name = "";
-  let greetMsg = ""
+  let greetMsg = "";
 
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+  async function greet() {
+    greetMsg = await invoke("greet", { name });
   }
 </script>
 
 <div>
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
+  <form on:submit|preventDefault={greet}>
+    <input
+      class="input p-2"
+      id="greet-input"
+      placeholder="Enter a name..."
+      bind:value={name}
+    />
+    <button class="btn variant-filled" type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
 </div>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
+// Your selected Skeleton theme:
+import "@skeletonlabs/skeleton/themes/theme-skeleton.css";
+// This contains the bulk of Skeletons required styles:
+import "@skeletonlabs/skeleton/styles/skeleton.css";
+// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+import "./app.postcss";
+// Standard styles
 import "./styles.css";
+
 import App from "./App.svelte";
 
 const app = new App({
