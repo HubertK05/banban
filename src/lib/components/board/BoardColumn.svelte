241186133ded5df2acd9d4efcd01b77cdbba348a
Adimac93 <adimac93@gmail.com>
2023-07-26 23:29:28 +0200

    💫 speed up flip animaion +sync activities count on consider

--- src/lib/components/board/BoardColumn.svelte
+++ src/lib/components/board/BoardColumn.svelte
@@ -13,7 +13,7 @@
 
     export let columnId: number;
     export let column: Column;
-    const flipDurationMs = 300;
+    const flipDurationMs = 125;
 
     $: {
         // WARNING! Updates every key stroke
@@ -135,7 +135,7 @@
 
         <span
             class="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30"
-            >{column.activities.size}</span
+            >{draggableActivities.length}</span
         >
         <button
             on:click={createActivity}
